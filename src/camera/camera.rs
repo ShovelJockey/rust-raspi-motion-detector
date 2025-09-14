83404b376c50a1aa5d2734b04e2f067f0e955008
Jamie <jamie.hirst10@gmail.com>
2025-09-14 12:13:28 +0100

    remove unused code

--- src/camera/camera.rs
+++ src/camera/camera.rs
@@ -100,63 +100,7 @@ pub fn start_stream_rtp() -> u32 {
     return camera_process_id;
 }
 
-#[allow(dead_code)]
-pub fn start_stream_hls() -> u32 {
-    let command_args = [
-        "-t",
-        "0",
-        "--inline",
-        "--signal",
-        "--listen",
-        "-o",
-        "tcp://localhost:8080",
-    ];
-    let child_process = Command::new("rpicam-vid")
-        .args(command_args)
-        .stderr(Stdio::piped())
-        .spawn()
-        .expect("Expected Camera command to succeed without error.");
-    return child_process.id();
-}
-
-#[allow(dead_code)]
-pub fn start_ffmpeg_hls_conversion(hls_dir: &TempDir) -> u32 {
-    let output_dir = hls_dir.path().display().to_string();
-    let child_process = Command::new("ffmpeg")
-        .args([
-            "-i",
-            "tcp://0.0.0.0:8080?listen", // Input from RPi
-            "-c:v",
-            "copy", // No re-encoding (low CPU)
-            "-f",
-            "hls", // Output HLS
-            "-hls_time",
-            "4", // 2-second segments
-            "-hls_list_size",
-            "6", // Keep 5 segments in playlist
-            "-hls_flags",
-            "delete_segments", // Auto-delete old segments
-            "-hls_segment_filename",
-            &format!("{}/segment_%03d.ts", output_dir),
-            &format!("{}/stream.m3u8", output_dir),
-        ])
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .spawn()
-        .expect("Failed to start FFmpeg");
-
-    return child_process.id();
-}
-
 pub fn shutdown_cam_process(camera_process_id: u32) {
-    Command::new("kill")
-        .args(["-SIGUSR2", camera_process_id.to_string().as_str()])
-        .output()
-        .expect("SIGUSR signal sent to camera thread");
-}
-
-#[allow(dead_code)]
-pub fn shutdown_ffmpeg_process(camera_process_id: u32) {
     Command::new("kill")
         .args(["-SIGUSR2", camera_process_id.to_string().as_str()])
         .output()
