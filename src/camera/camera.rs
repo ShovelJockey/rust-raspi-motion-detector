7bf2a5eed4bfff8908c50fe0dadc6727b6bbf19f
Jamie Hirst <jamie.hirst10@gmail.com>
2026-08-29 14:51:00 +0100

    update cam args, heavily trim logging spam

--- src/camera/camera.rs
+++ src/camera/camera.rs
@@ -62,8 +62,6 @@ pub fn start_stream_rtp() -> u32 {
         "--inline",
         "--listen",
         "--low-latency",
-        "--width 1280",
-        "--height 720",
         "--libav-format",
         "h264",
         "-o",
@@ -73,29 +71,26 @@ pub fn start_stream_rtp() -> u32 {
     let camera_process = Command::new("rpicam-vid")
         .args(command_args)
         .stdout(Stdio::piped())
-        .stderr(Stdio::null())
         .spawn()
         .expect("Expected Camera command to succeed without error.");
     let camera_process_id = camera_process.id();
 
     let ffmpeg_args = [
-        "-f",
-        "rtp",
-        "-c:v",
-        "libx264",
         "-i",
         "-",
+        "-c:v",
+        "libx264",
         "-preset",
         "ultrafast",
         "-tune",
         "zerolatency",
+        "-f",
+        "rtp",
         "rtp://127.0.0.1:5004",
     ];
     Command::new("ffmpeg")
         .args(ffmpeg_args)
         .stdin(Stdio::from(camera_process.stdout.unwrap()))
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
         .spawn()
         .expect("FFMPEG video processing process completed successfully.");
 
